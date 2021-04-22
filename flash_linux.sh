2d34edc2949b8c78d0cda198cff27b07a77121ed
Chiel Prins <snirpo@gmail.com>
2021-04-22 22:36:59 +0200

    use lsusb to wait for device

--- flash_linux.sh
+++ flash_linux.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 while true
 do
-  read -p "Press enter to start flashing..."
+  echo "Waiting for device"
+  while ! lsusb 2> /dev/null | grep "2207:330e" -q; do
+    sleep 1
+  done
+
+  sleep 1
   ./rkdeveloptool_linux db rk3308_loader_uart0_m0_emmc_port_support_sd_20190717.bin &&
     sleep 2 &&
     ./rkdeveloptool_linux wl 0x40 "idbloader.img" &&
     ./rkdeveloptool_linux rd
+  echo -e "\a"
+  sleep 2
 done
