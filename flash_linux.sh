1863d8705d17852b998be8489a241eab78ec3243
Chiel Prins <chiel@cpeb.it>
2021-08-11 18:50:42 +0200

    improvements

--- flash_linux.sh
+++ flash_linux.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 set -e
 
+usb_pid=""
+
+wait_for_device() {
+  while ! lsusb 2>/dev/null | grep "2207:$usb_pid" -q; do
+    sleep 1
+  done
+}
+
+case $1 in
+"rk3308") usb_pid="330e" ;;
+"rv1109") usb_pid="110b" ;;
+*)
+  echo "Invalid device type: $1"
+  exit 1
+  ;;
+esac
+
 echo "Waiting for device..."
-
-while ! lsusb 2> /dev/null | grep "2207:110b" -q; do
-  sleep 1
-done
-
+wait_for_device
 echo "Device found."
-
 sleep 1
-
-sudo ./bin/rkdeveloptool_linux db ./bin/rv1126_loader_v1.05.106.bin
-
+sudo ./bin/rkdeveloptool_linux db ./bin/"$1".bin
 echo "Waiting for device to re-enumerate."
-while ! lsusb 2> /dev/null | grep "2207:110b" -q; do
-  sleep 1
-done
-
-sudo ./bin/rkdeveloptool_linux wl 0 "$1"
+wait_for_device
+sudo ./bin/rkdeveloptool_linux wl 0 "$2"
 sudo ./bin/rkdeveloptool_linux rd
-
 echo "Device flashed successfully."
--- flash_windows.bat
+++ flash_windows.bat
@@ -1,11 +1,21 @@
 @echo off
 cls
+
+if "%1"=="rk3308" (
+  set usb_pid="330E"
+) else if "%1"=="rv1109" (
+  set usb_pid="111B"
+) else (
+  echo Invalid device type %1
+  exit /b 1
+)
+
 echo Waiting for device...
-powershell -c "while ($(GET-WMIOBJECT Win32_PnPEntity | Where { $_.DeviceID -like 'USB\VID_2207&PID_110B*' } | measure).count -eq 0) {Start-Sleep -s 1}"
+powershell -c "while ($(GET-WMIOBJECT Win32_PnPEntity | Where { $_.DeviceID -like 'USB\VID_2207&PID_%usb_pid%*' } | measure).count -eq 0) {Start-Sleep -s 1}"
 echo Device found.
 .\bin\rkdeveloptool.exe db .\bin\rv1126_loader_v1.05.106.bin
 echo Waiting for device to re-enumerate.
-powershell -c "while ($(GET-WMIOBJECT Win32_PnPEntity | Where { $_.DeviceID -like 'USB\VID_2207&PID_110B*' } | measure).count -eq 0) {Start-Sleep -s 1}"
+powershell -c "while ($(GET-WMIOBJECT Win32_PnPEntity | Where { $_.DeviceID -like 'USB\VID_2207&PID_%usb_pid%*' } | measure).count -eq 0) {Start-Sleep -s 1}"
 .\bin\rkdeveloptool.exe wl 0 %1
 .\bin\rkdeveloptool.exe rd
 echo Device flashed successfully.
